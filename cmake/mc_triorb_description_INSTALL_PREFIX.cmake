3dfde456a461aef39d2f2c74af9fe7656cd2752d
Khang Pham <phamthekhang632@gmail.com>
2026-09-14 08:29:28 +0200

    chore: apply pre-commit

--- cmake/mc_triorb_description_INSTALL_PREFIX.cmake
+++ cmake/mc_triorb_description_INSTALL_PREFIX.cmake
@@ -1,2 +1,3 @@
 set(mc_triorb_description_INSTALL_PREFIX "${mc_triorb_description_DIR}/../")
-get_filename_component(mc_triorb_description_INSTALL_PREFIX "${mc_triorb_description_INSTALL_PREFIX}" REALPATH)
+get_filename_component(mc_triorb_description_INSTALL_PREFIX
+                       "${mc_triorb_description_INSTALL_PREFIX}" REALPATH)
